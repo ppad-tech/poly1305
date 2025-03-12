569dbfcc9e9952a74b8b45d582ad4bd88d070748
Jared Tobin <jared@jtobin.io>
2025-03-12 11:48:58 +0400

    bench: small vs big key

--- bench/Main.hs
+++ bench/Main.hs
@@ -18,13 +18,18 @@ msg :: BS.ByteString
 msg = fromJust . B16.decode $
   "4c616469657320616e642047656e746c656d656e206f662074686520636c617373206f66202739393a204966204920636f756c64206f6666657220796f75206f6e6c79206f6e652074697020666f7220746865206675747572652c2073756e73637265656e20776f756c642062652069742e"
 
-key :: BS.ByteString
-key = fromJust . B16.decode $
-  "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+key_small :: BS.ByteString
+key_small = fromJust . B16.decode $
+  "0000000000000000000000000000000000000000000000000000000000000003"
+
+key_big :: BS.ByteString
+key_big = fromJust . B16.decode $
+  "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff3"
 
 suite :: Benchmark
 suite =
   bgroup "ppad-poly1305" [
-    bench "mac" $ nf (Poly1305.mac key) msg
+    bench "mac (small key)" $ nf (Poly1305.mac key_small) msg
+  , bench "mac (big key)" $ nf (Poly1305.mac key_big) msg
   ]
 
